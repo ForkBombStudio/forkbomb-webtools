a7749cdc4011caa85fd8d4eb516abb1e86234d48
donghch <dieshenke@gmail.com>
2025-06-17 15:39:03 -0700

    use file name insted of file

--- src/app/components/file-card.tsx
+++ src/app/components/file-card.tsx
@@ -1,8 +1,8 @@
 
-export default function FileCard({ file, onDelete }: { file: File, onDelete: () => void }) {
+export default function FileCard({ file, onDelete }: { file: string, onDelete: () => void }) {
     return (
         <div className="flex flex-row border rounded p-1 justify-between">
-            <p>{ file.name }</p>
+            <p>{ file }</p>
             <button onClick={onDelete} className="text-red-500 hover:text-red-700">
                 Delete
             </button>
